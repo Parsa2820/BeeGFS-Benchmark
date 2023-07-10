d0af279e580c75f55bf72b7bcf7d46cd4d18b197
Parsa Mohammadian <parsa2820@gmail.com>
2023-07-10 23:24:16 +0330

    Change iozone command

--- benchmark/IOZone.sh
+++ benchmark/IOZone.sh
@@ -1,5 +1,6 @@
 FILE=$1
 LOG_FILE=$2
+SIZE=$3
 EXCEL_FILE="$FILE.xls"
 echo "Starting IOZone benchmark on $HOSTNAME" > $LOG_FILE
 echo "File: $FILE" >> $LOG_FILE
@@ -7,4 +8,4 @@ echo "Date: $(date)" >> $LOG_FILE
 echo "Excel file: $EXCEL_FILE" >> $LOG_FILE
 echo "========================================" >> $LOG_FILE
 echo "Benchmarking with all tests with file sizes from 1MB to 10GB" >> $LOG_FILE
-iozone -a -f $FILE -b $EXCEL_FILE -n 1m -g 10g >> $LOG_FILE
+iozone -a -f $FILE -b $EXCEL_FILE -s $SIZE >> $LOG_FILE
