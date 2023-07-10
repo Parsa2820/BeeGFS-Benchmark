e762d40e0e3b387051ec0773092e8ff99d459203
Parsa Mohammadian <parsa2820@gmail.com>
2023-07-10 17:14:15 +0330

    Fix iozone command

--- benchmark/IOZone.sh
+++ benchmark/IOZone.sh
@@ -7,4 +7,4 @@ echo "Date: $(date)" >> $LOG_FILE
 echo "Excel file: $EXCEL_FILE" >> $LOG_FILE
 echo "========================================" >> $LOG_FILE
 echo "Benchmarking with all tests with file sizes from 1MB to 10GB" >> $LOG_FILE
-iozone -a -F $FILE -b $EXCEL_FILE -n 1m -g 10g >> $LOG_FILE
+iozone -a -f $FILE -b $EXCEL_FILE -n 1m -g 10g >> $LOG_FILE
